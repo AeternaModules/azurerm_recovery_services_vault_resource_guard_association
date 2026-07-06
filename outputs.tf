ff3fa981bf8592856d04e4af4ef07dd999a9d7e1
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:36 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,12 @@ output "recovery_services_vault_resource_guard_associations" {
   description = "All recovery_services_vault_resource_guard_association resources"
   value       = azurerm_recovery_services_vault_resource_guard_association.recovery_services_vault_resource_guard_associations
 }
+output "recovery_services_vault_resource_guard_associations_resource_guard_id" {
+  description = "List of resource_guard_id values across all recovery_services_vault_resource_guard_associations"
+  value       = [for k, v in azurerm_recovery_services_vault_resource_guard_association.recovery_services_vault_resource_guard_associations : v.resource_guard_id]
+}
+output "recovery_services_vault_resource_guard_associations_vault_id" {
+  description = "List of vault_id values across all recovery_services_vault_resource_guard_associations"
+  value       = [for k, v in azurerm_recovery_services_vault_resource_guard_association.recovery_services_vault_resource_guard_associations : v.vault_id]
+}
 
