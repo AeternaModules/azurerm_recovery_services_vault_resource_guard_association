04709a44e8491ce58b47b3b8e7a5bfbe46e83874
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:52 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "recovery_services_vault_resource_guard_associations" {
-  description = "All recovery_services_vault_resource_guard_association resources"
-  value       = azurerm_recovery_services_vault_resource_guard_association.recovery_services_vault_resource_guard_associations
-}
 output "recovery_services_vault_resource_guard_associations_resource_guard_id" {
-  description = "List of resource_guard_id values across all recovery_services_vault_resource_guard_associations"
-  value       = [for k, v in azurerm_recovery_services_vault_resource_guard_association.recovery_services_vault_resource_guard_associations : v.resource_guard_id]
+  description = "Map of resource_guard_id values across all recovery_services_vault_resource_guard_associations, keyed the same as var.recovery_services_vault_resource_guard_associations"
+  value       = { for k, v in azurerm_recovery_services_vault_resource_guard_association.recovery_services_vault_resource_guard_associations : k => v.resource_guard_id }
 }
 output "recovery_services_vault_resource_guard_associations_vault_id" {
-  description = "List of vault_id values across all recovery_services_vault_resource_guard_associations"
-  value       = [for k, v in azurerm_recovery_services_vault_resource_guard_association.recovery_services_vault_resource_guard_associations : v.vault_id]
+  description = "Map of vault_id values across all recovery_services_vault_resource_guard_associations, keyed the same as var.recovery_services_vault_resource_guard_associations"
+  value       = { for k, v in azurerm_recovery_services_vault_resource_guard_association.recovery_services_vault_resource_guard_associations : k => v.vault_id }
 }
 
